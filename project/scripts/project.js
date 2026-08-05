9ce23f5e23a4706fb585e6bd28603a6189b6c8b9
sstarkey84 <sstarkey84@users.noreply.guthub.com>
2026-08-04 18:40:33 -0700

    week 6 project

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -20,3 +20,4 @@ currentYear.textContent = new Date().getFullYear();
 const lastModified = document.querySelector("#lastModified");
 lastModified.textContent = "Last Modification: " + document.lastModified;
 
+// This is a comment //
